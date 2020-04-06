abfdb7d13811c987f05fea62c4642e300b526af2
MiguelAngel0201 <mpena.creactiva@gmail.com>
2020-04-06 14:14:03 -0400

    Actualizando cambios

--- vendors/modal/modal.js
+++ vendors/modal/modal.js
@@ -1,8 +1,8 @@
 // https://www.w3docs.com/snippets/javascript/how-to-create-a-modal-dialog-box-with-css-and-javascript.html
 var modal = document.querySelector('.modal');
 var trigger = document.querySelectorAll('.btn-play');
+var triggerBtn = document.querySelectorAll('.btn-modal');
 var closeButton = document.querySelector('.close-button');
-
 function toggleModal() {
   modal.classList.toggle('show-modal');
 }
@@ -12,6 +12,7 @@ function windowOnClick(event) {
     toggleModal();
   }
 }
+triggerBtn.forEach(el => el.addEventListener('click', toggleModal))
 trigger.forEach(el => el.addEventListener('click', toggleModal));
-closeButton.addEventListener('click', toggleModal);
+// closeButton.addEventListener('click', toggleModal);
 window.addEventListener('click', windowOnClick);
